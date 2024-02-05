300696cb3747688347a23515904218463b7d41b9
evan-hatley <143963975+evan-hatley@users.noreply.github.com>
2024-02-05 14:27:34 -0600

    Update server.js

--- server.js
+++ server.js
@@ -8,3 +8,9 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(routes);
+
+db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`API server for ${activity} running on port ${PORT}!`);
+    });
+  });
